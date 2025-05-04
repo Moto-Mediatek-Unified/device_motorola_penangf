ShivamX087/android_device_motorola_vicky

vicky: Initial skeleton tree
Signed-off-by: claxten10 <claxten10@gmail.com>
Signed-off-by: ShivamX087 <shivamx976@gmail.com>

Android.mk
@@ -1,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),vicky)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif

AndroidProducts.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_vicky.mk

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/vicky
+
+# Inherit the proprietary files
+include vendor/motorola/vicky/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,15 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/vicky/vicky-vendor.mk)

Moto-Mediatek-Unified/device_motorola_penangf

Android.mk
@@ -1,0 +1,11 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),penangf)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif

AndroidProducts.mk
@@ -1,0 +1,8 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_penangf.mk

BoardConfig.mk
@@ -1,0 +1,10 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/motorola/penangf
+
+# Inherit the proprietary files
+include vendor/motorola/penangf/BoardConfigVendor.mk

device.mk
@@ -1,0 +1,15 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 31
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/motorola/penangf/penangf-vendor.mk)